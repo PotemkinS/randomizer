86b78e7bc09d884fa01a3cf8211d7ac9172315fd
PotemkinS <sergey.potemkin@urfu.me>
2025-02-24 03:16:19 +0500

    new features layout

---
+++ bring_back_original_everything.py
@@ -0,0 +1,7 @@
+import os
+
+os.chdir('generate_info')
+file = '..\\national ideas\\bring_back_original_national_ideas.exe'
+os.system('"' + file + '"')
+file = '..\\ideagroups\\bring_back_original_idea_groups.exe'
+os.system('"' + file + '"')
